6a0aa6c1af1580b48e82f18b81cbbb4c0f5731d3
spitzgoby <tomleu@gmail.com>
2015-05-06 22:29:52 +0800

    Add installation commands, remove hadoopy from requirments

--- bin/install.sh
+++ bin/install.sh
@@ -1,1 +1,13 @@
-sudo pip install -r requirements.txt 
+# CLONE GIT REPOR
+echo 'Creating home directory'
+export E63_HOME='~/e63_final';
+mkdir $E63_HOME &&
+cd $E63_HOME &&
+git clone git@github.com:spitzgoby/e63_final_proj.git &&
+
+# INSTALL PYTHON DEPENDENCIES AND START SERVICE
+echo 'Installing required python modules';
+sudo pip install -r requirements.txt &&
+python interaction_service/interaction_service.py;
+
+# CONFIGURE ENVIRONMENT FOR SPARK 
