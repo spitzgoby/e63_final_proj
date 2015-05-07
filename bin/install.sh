53d489facda097b3dc2e7bd541f7e9c4766413a9
spitzgoby <tomleu@gmail.com>
2015-05-07 20:35:57 +0800

    Update install script

--- bin/install.sh
+++ bin/install.sh
@@ -1,9 +1,7 @@
 # CLONE GIT REPOR
 echo 'Creating home directory'
-export E63_HOME='~/e63_final';
-mkdir $E63_HOME &&
-cd $E63_HOME &&
-git clone git@github.com:spitzgoby/e63_final_proj.git &&
+git clone https://github.com/spitzgoby/e63_final_proj.git &&
+cd ~/e63_final_proj; 
 
 # INSTALL PYTHON DEPENDENCIES AND START SERVICE
 echo 'Installing required python modules';
