93d903028eea77c0dbd7b4a873e5c8377435b9da
spitzgoby <tomleu@gmail.com>
2015-05-07 20:34:34 +0800

    Fix bug with cql and .format() collision on {} characters

--- interaction_service/interaction_db.py
+++ interaction_service/interaction_db.py
@@ -6,32 +6,29 @@ TABLE_NAME = 'interactions'
 
 session = cassandra.cluster.Cluster().connect()
 
-session.execute('''CREATE KEYSPACE IF NOT EXISTS {0} 
-                    WITH REPLICATION = { 'class': 'SimpleStrategy',
-                                          'replication_factor': 1 };
-                '''.format(KEYSPACE_NAME))
+session.execute('''CREATE KEYSPACE IF NOT EXISTS %s
+                    WITH REPLICATION = 
+                      { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };
+                ''' % (KEYSPACE_NAME))
 
-session.execute('''CREATE TABLE IF NOT EXISTS {0}.{1} (
+session.execute('''CREATE TABLE IF NOT EXISTS %s.%s (
                     user_id uuid,
                     time_stamp double,
                     duration double,
                     latitude double,
                     longitude double,
                     PRIMARY KEY (user_id, time_stamp)
-                  )'''.format(KEYSPACE_NAME, TABLE_NAME))
+                  )''' % (KEYSPACE_NAME, TABLE_NAME))
 
 prepared = session.prepare('''INSERT INTO e63.interactions
             (user_id, time_stamp, duration, latitude, longitude)
             VALUES (?, ?, ?, ?, ?)''')
 def insertInteraction(i):
-  query = '''INSERT INTO interactions
-            (user_id, time_stamp, duration, latitude, longitude)
-            VALUES ("{0}", {1}, {2}, {3}, {4})'''.format(
-              i.user_id,
-              i.time_stamp,
-              i.duration,
-              i.coords.latitude, 
-              i.coords.longitude)
+  query = '''INSERT INTO interactions (user_id, time_stamp, duration, latitude, longitude)
+              VALUES (\"%s\", %s, %s, %s, %s)''' \
+              % (i.user_id, i.time_stamp, i.duration, i.coords.latitude, i.coords.longitude)
   logging.getLogger().debug("Query: " + query)
   session.execute(prepared, 
     (i.user_id, i.time_stamp, i.duration, i.coords.latitude, i.coords.longitude))
+session.execute("CREATE KEYSPACE Excelsior \
+  WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };")
