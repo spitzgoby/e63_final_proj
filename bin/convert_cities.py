eed9e523acd1f90f5c143f6fde6228c3cedcc1b7
spitzgoby <tomleu@gmail.com>
2015-05-12 19:13:50 +0800

    Fix gitignore, fix convert_cities bug, install.sh should be ignored for now

--- bin/convert_cities.py
+++ bin/convert_cities.py
@@ -1,20 +1,18 @@
-#! finalenv/bin/python
+#! ../finalenv/bin/python
 
-importantTokenIndices = [0, 1, 4, 5, 8, 14]
-# GEONAME_ID = 0
+importantTokenIndices = [1, 4, 5, 8]
 # NAME = 1
 # LATITUDE = 4
 # LONGITUDE = 5
 # COUNTRY_CODE = 8
-# POPULATION = 14
 
 with open('cities.csv', 'w') as outfile:
   with open('cities15000.txt') as infile:
     for line in infile:
       tokens = line.split('\t')
-      if tokens[6] == 'P':
-        writeTokens = []
-        for num in importantTokenIndices:
-          writeTokens.append(tokens[num])
-        print writeTokens[1]
-        outfile.write(','.join(writeTokens) + '\n')
+      writeTokens = []
+      for num in importantTokenIndices:
+        # remove commas from city names
+        writeTokens.append(tokens[num].replace(',', ''))
+      print writeTokens[0]
+      outfile.write(','.join(writeTokens) + '\n')
--- bin/install.sh
+++ bin/install.sh
@@ -1,11 +1,48 @@
-# CLONE GIT REPOR
-echo 'Creating home directory'
-git clone https://github.com/spitzgoby/e63_final_proj.git &&
-cd ~/e63_final_proj; 
+function usage
+{
+  echo "Usage: ./install.sh [-k keyfile] [-d aws ip/host]"
+}
 
-# INSTALL PYTHON DEPENDENCIES AND START SERVICE
-echo 'Installing required python modules';
-sudo pip install -r requirements.txt &&
-python interaction_service/interaction_service.py;
+while [ "$1" != "" ]; do
+    case $1 in
+        -k | --key )            shift
+                                KEY=$1
+                                ;;
+        -d | --dns )            shift
+                                HOST=$1 
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
 
-# CONFIGURE ENVIRONMENT FOR SPARK 
+if [ "$KEY" != "" ]; then
+  if [ "$HOST" != "" ]; then
+    ssh -t -i $KEY ubuntu@$HOST bash -c "'
+      # CLONE GIT REPO
+      echo 'Creating home directory'
+      git clone https://github.com/spitzgoby/e63_final_proj.git &&
+      cd ~/e63_final_proj; 
+
+      # INSTALL PYTHON DEPENDENCIES AND START SERVICE
+      echo 'Installing required python modules';
+      sudo pip install -r requirements.txt &&
+      echo 'Starting interaction REST service';
+      python interaction_service/interaction_service.py;
+
+      # CONFIGURE ENVIRONMENT FOR SPARK 
+    '"
+  else
+    usage
+    exit 1
+  fi
+else
+  usage
+  exit 1
+fi
+
+exit
