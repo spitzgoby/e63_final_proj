6cbcc95fe5134d19321549da8599be6d75a8c524
spitzgoby <tomleu@gmail.com>
2015-05-12 20:06:12 +0800

    Add documentation and remove debug statement

--- spark/src/main/scala-2.11/e63/thomasleu/final_asn/InteractionCities.scala
+++ spark/src/main/scala-2.11/e63/thomasleu/final_asn/InteractionCities.scala
@@ -13,21 +13,26 @@ object InteractionCities {
   case class InteractionCity(user_id: UUID, time_stamp: Double, duration: Double, city: City)
 
   def main(args: Array[String]): Unit = {
+    // prepare Spark to use local Cassandra DB
     val conf = new SparkConf()
       .setMaster("local")
       .setAppName("InteractionCities")
       .set("spark.cassandra.connection.host", "127.0.0.1")
     val sc = new SparkContext(conf)
 
+    // grab city data from Cassandra and build a k-d tree
     val citiesRDD = sc.cassandraTable[City]("e63", "cities")
     val cities = citiesRDD.collect().toList
     val cityTree = new CityKDTreeFactory().kdTree(cities)
 
+    // grab interaction data from Cassandra and map cities to them. For this job there is no
+    // reduce phase.
     val interactions = sc.cassandraTable[Interaction]("e63", "interactions")
     val interactionCities = interactions.map(x => {
         InteractionCity(x.user_id, x.time_stamp, x.duration, nearestCity(x, cityTree))
     })
 
+    // save the results
     interactionCities.saveAsTextFile("InteractionCities")
   }
 
@@ -45,14 +50,17 @@ object InteractionCities {
   }
 
   private def nearestCity(interaction: Interaction, cities: CityKDTree, depth: Int) : City = {
+    // base case, leaf node is current best
     if (cities.isleaf) {
       cities.root
     } else {
-      if (cities.root.name == "Beijing" || cities.root.name == "Qinhuangdao") {
-        val pause = 0
-      }
 
+      // declare and initialize variables so they can be accessed within if statements
       var nextChild , otherChild : CityKDTree = null
+      var minimumDistance = Double.MaxValue
+      var closest : City = null
+
+      // determine which direction to search initially
       if (traverseLeft(interaction, cities.root, depth)) {
         nextChild = cities.leftChild
         otherChild = cities.rightChild
@@ -61,28 +69,25 @@ object InteractionCities {
         otherChild = cities.leftChild
       }
 
-      // prepare variables and values
-      var minimumDistance = Double.MaxValue
-      var closest : City = null
-
-      // recursively check next child (if exists)
+      // recursively check next child (if it exists) and set minimum value to this city's value
       if (nextChild != null) {
         closest = nearestCity(interaction, nextChild, depth + 1)
-        minimumDistance = locationDistance(interaction, closest)
+        minimumDistance = locationDistanceSquared(interaction, closest)
       }
 
-
       // if distance to hyperplane is less than distance from current closest city check other child
-      if (minimumDistance >= planeDistance(interaction, cities.root, depth) && otherChild != null) {
+      if (minimumDistance >= planeDistanceSquared(interaction, cities.root, depth) && otherChild != null) {
+        // the distance to the split plane is less than the current minimum distance so the closest city could
+        // be in the other tree
         val otherClosest = nearestCity(interaction, otherChild, depth + 1)
-        val otherDistance = locationDistance(interaction, otherClosest)
+        val otherDistance = locationDistanceSquared(interaction, otherClosest)
         if (otherDistance < minimumDistance) {
           closest = otherClosest
         }
       }
 
-      // check the current city against the current best
-      val nodeDistance = locationDistance(interaction, cities.root)
+      // finally, check the current city against the current best
+      val nodeDistance = locationDistanceSquared(interaction, cities.root)
       if (nodeDistance < minimumDistance) {
         closest = cities.root
       } else if (nodeDistance == minimumDistance) {
@@ -103,15 +108,23 @@ object InteractionCities {
   }
 
 
-  def planeDistance(interaction: Interaction, city: City, depth : Int) : Double = {
-    if (depth % 2 == 0) {
+  private def planeDistanceSquared(interaction: Interaction, city: City, depth : Int) : Double = {
+    // the square is returned for efficiency reasons
+    if (depth % 2 == 0) { // use latitude
       Math.pow(interaction.latitude - city.latitude, 2)
-    } else {
+    } else { // use longitude
       Math.pow(interaction.longitude - city.longitude, 2)
     }
   }
 
-  def locationDistance(interaction: Interaction, city: City) : Double = {
+  /** *
+    * Calculates the square of the distance between an interaction and a city.
+    * @param interaction The user interaction with which to find distance
+    * @param city The city from which to find distance
+    * @return The square of the distance between the city and the interaction
+    */
+  def locationDistanceSquared(interaction: Interaction, city: City) : Double = {
+    // the square is returned for efficiency reasons
     math.pow(interaction.latitude - city.latitude, 2) + math.pow(interaction.longitude - city.longitude, 2)
   }
 }
