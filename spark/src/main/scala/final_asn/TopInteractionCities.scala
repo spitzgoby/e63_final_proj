1eb789c9d6aee7bf591502e5ab8cbb9779c6c1cf
spitzgoby <tomleu@gmail.com>
2015-05-14 06:48:07 +0800

    Move kd tree into TopInteractionCities code

--- spark/src/main/scala/final_asn/TopInteractionCities.scala
+++ spark/src/main/scala/final_asn/TopInteractionCities.scala
@@ -1,6 +1,5 @@
 package final_asn
 
-import InteractionCities.Interaction
 import final_asn.InteractionCities.Interaction
 import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.SparkContext._
@@ -9,6 +8,57 @@ import com.datastax.spark.connector._
 /**
  * Created by Thomas Leu
  */
+
+case class City (name: String, latitude: Double, longitude: Double, country: String)
+case class CityKDTree(root: City, leftChild: CityKDTree, rightChild: CityKDTree) {
+  def isleaf : Boolean = (leftChild == null) && (rightChild == null)
+  def count : Int = {
+    (leftChild, rightChild) match {
+      case (null, null) => 1
+      case (l, null) => l.count + 1
+      case (null, r) => r.count + 1
+      case (l, r) => l.count + r.count + 1
+    }
+  }
+}
+
+class CityKDTreeFactory {
+  /** *
+    * Creates a CityKDTree from a list of City objects
+    * @param cities The list of cities to make into a CityKDTree
+    * @return A CityKDTree with the cities split by latitude on even-leveled nodes and longitude
+    *         on odd-leveled nodes
+    * @see http://en.wikipedia.org/wiki/K-d_tree
+    */
+  def kdTree(cities: List[City]) : CityKDTree = {
+    kdTree(cities, 0)
+  }
+
+  private def kdTree(cities: List[City], depth: Int) : CityKDTree = {
+    cities.length match {
+      case 0 => null // no more cities
+      case 1 => // leaf node
+        new CityKDTree(
+          root = cities.head,
+          leftChild = null,
+          rightChild = null
+        )
+      case _ => // branch node
+        // sort cities by latitude on even branches and longitude on odd branches
+        val sortedCities = cities.sortWith((x, y) => {
+          if (depth % 2 == 0) x.latitude < y.latitude
+          else x.longitude < y.longitude})
+
+        val median = cities.length / 2
+
+        new CityKDTree( // set root to median value and split left and right side into planes
+          root = sortedCities(median),
+          leftChild = kdTree(sortedCities.slice(0,median), depth + 1),
+          rightChild = kdTree(sortedCities.slice(median+1, cities.length), depth + 1))
+    }
+  }
+}
+
 object TopInteractionCities {
   def main(args: Array[String]) {
     var targetCountry = "world"
