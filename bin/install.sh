678a6b48425fe3d1adaea4c6cf6de760c62d28a3
spitzgoby <tomleu@gmail.com>
2015-05-14 03:51:21 +0800

    Update install scripts

--- bin/install.sh
+++ bin/install.sh
@@ -1,48 +1,37 @@
-function usage
-{
-  echo "Usage: ./install.sh [-k keyfile] [-d aws ip/host]"
-}
+# CLONE GIT REPO
+# git clone https://github.com/spitzgoby/e63_final_proj.git &&
+cd ~/e63_final_proj;
+mkdir data;
 
-while [ "$1" != "" ]; do
-    case $1 in
-        -k | --key )            shift
-                                KEY=$1
-                                ;;
-        -d | --dns )            shift
-                                HOST=$1 
-                                ;;
-        -h | --help )           usage
-                                exit
-                                ;;
-        * )                     usage
-                                exit 1
-    esac
-    shift
-done
+# add sbt repo to apt-get's database and install
+echo "deb http://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list &&
+sudo apt-get update;
+sudo apt-get install sbt;
 
-if [ "$KEY" != "" ]; then
-  if [ "$HOST" != "" ]; then
-    ssh -t -i $KEY ubuntu@$HOST bash -c "'
-      # CLONE GIT REPO
-      echo 'Creating home directory'
-      git clone https://github.com/spitzgoby/e63_final_proj.git &&
-      cd ~/e63_final_proj; 
+# CONFIGURE ENVIRONMENT FOR SPARK 
+cd ~;
+mkdir spark;
+cd spark/;
+wget http://mirror.tcpdiag.net/apache/spark/spark-1.2.1/spark-1.2.1.tgz;
+gunzip -c spark-1.2.1.tgz | tar -xvf -;
+cd spark-1.2.1/;
+sudo sbt/sbt assembly;
+cd ..;
+sudo cp -Rp spark-1.2.1 /usr/local/;
+cd /usr/local/;
+sudo ln -s spark-1.2.1 spark;
 
-      # INSTALL PYTHON DEPENDENCIES AND START SERVICE
-      echo 'Installing required python modules';
-      sudo pip install -r requirements.txt &&
-      echo 'Starting interaction REST service';
-      python interaction_service/interaction_service.py;
 
-      # CONFIGURE ENVIRONMENT FOR SPARK 
-    '"
-  else
-    usage
-    exit 1
-  fi
-else
-  usage
-  exit 1
-fi
+# copy existing interactions and and cities data
+cd ~/e63_final_proj;
+curl -k https://s3-us-west-1.amazonaws.com/thomasleu.e63final/interactions.csv -o ~/e63_final_proj/data/interactions.csv;
+curl -k https://s3-us-west-1.amazonaws.com/thomasleu.e63final/cities.csv -o ~/e63_final_proj/data/cities.csv;
 
-exit
+
+# INSTALL PYTHON DEPENDENCIES AND START SERVICE
+echo 'Installing required python modules';
+sudo pip install -r requirements.txt &&
+python bin/setup_cassandra.py;
+
+echo 'Starting interaction REST service';
+python interaction_service/interaction_service.py;
---
+++ bin/setup_cassandra.py
@@ -0,0 +1,37 @@
+from system import stderr
+import logging
+import cassandra.cluster
+
+logging.basicConfig(stream=stderr, level=logging.DEBUG)
+session = cassandra.cluster.Cluster().connect()
+
+session.execute('''CREATE KEYSPACE IF NOT EXISTS e63
+                    WITH REPLICATION =
+                    { 'class' : 'SimpleStrategy', 'replication_factor' : 1};
+                ''')
+
+session.execute('''CREATE TABLE IF NOT EXISTS e63.interactions (
+                    user_id text,
+                    time_stamp double,
+                    duration double,
+                    latitude double,
+                    longitude double,
+                    PRIMARY KEY (user_id, time_stamp));
+                ''')
+
+session.execute('''COPY e63.interactions 
+                    (user_id, time_stamp, duration, latitude, longitude) 
+                    from 'interactions.csv';
+                ''')
+
+session.execute('''CREATE TABLE IF NOT EXISTS e63.cities (
+                    name varchar PRIMARY KEY,
+                    country varchar,
+                    latitude double,
+                    longitude double,);
+                ''')
+
+session.execute('''COPY e63.cities 
+                    (name, latitude, longitude, country)
+                    from 'cities.csv';
+                ''')
