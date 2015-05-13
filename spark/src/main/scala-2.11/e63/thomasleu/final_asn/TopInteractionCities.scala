4f7f82ecc488bf86d758723d4883dee5d6460fd8
spitzgoby <tomleu@gmail.com>
2015-05-13 23:03:04 +0800

    Add spark job for finding top interaction cities of a country

--- spark/src/main/scala-2.11/e63/thomasleu/final_asn/TopInteractionCities.scala
+++ spark/src/main/scala-2.11/e63/thomasleu/final_asn/TopInteractionCities.scala
@@ -1,16 +1,22 @@
 package e63.thomasleu.final_asn
 
 import e63.thomasleu.final_asn.InteractionCities.Interaction
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkConf
+import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.SparkContext._
 import com.datastax.spark.connector._
 
 /**
  * Created by Thomas Leu
  */
-object AverageUserDurationPerCity {
+object TopInteractionCities {
   def main(args: Array[String]) {
+    if (args.length < 1) {
+      println("Not enough arguments, please include Country's  ISO 3166 code")
+      System.exit(1)
+    }
+
+    val targetCountry = args(0)
+
     // prepare Spark to use local Cassandra DB
     val conf = new SparkConf()
       .setMaster("local")
@@ -25,18 +31,17 @@ object AverageUserDurationPerCity {
 
     // grab interaction data from Cassandra and map cities to them to (City, duration) pairs
     val interactions = sc.cassandraTable[Interaction]("e63", "interactions")
-    val avgCityDurations = interactions.map(x => {
+    val countryInteractions = interactions.map(x => {
       (InteractionCities.nearestCity(x, cityTree), x.duration)
     }).combineByKey(
         (duration: Double) => (duration, 1),
         (acc: (Double, Int), duration: Double) => (acc._1 + duration, acc._2 + 1),
         (acc1: (Double, Int), acc2: (Double, Int)) => (acc1._1 + acc2._1, acc1._2 + acc2._2)
-      ).filter{ case (_, (_, num_interactions)) => num_interactions > 2}
-      .map{case (city, (total_dur, num_interactions)) => (city.name, city.country, total_dur / num_interactions)}
+      ).filter{ case (city, (_, _)) => city.country.equalsIgnoreCase(targetCountry)}
+      .map{case (city, (total_dur, num_interactions)) => (city.name, city.country, city.latitude, city.longitude, num_interactions)}
     // print all cities with interactions into a comma separated value file sorted by country
-    avgCityDurations.sortBy{ case (_, country, _) => country}
-      .map{ case (name, country, avgDuration) => Array(name, country, avgDuration).mkString(",")}
-      .saveAsTextFile("average_city_duration")
+    countryInteractions.sortBy{ case (_, _, _, _, num_interactions) => -num_interactions}
+      .map{ case (name, country, latitude, longitude, num_interacitons) => Array(name, country, latitude, longitude, num_interacitons).mkString(",")}
+      .saveAsTextFile("top_cities_" + targetCountry)
   }
-
 }
